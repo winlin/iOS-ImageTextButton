137d556aeba61eff4c5abf52d259caa58dd0046d
winlin <pcliuguangtao@163.com>
2013-05-18 15:34:44 +0800

    Create UIButton+ImageText.h

---
+++ UIButton+ImageText.h
@@ -0,0 +1,14 @@
+//
+//  UIButton+ImageText.h
+//  BaccaratClient
+//
+//  Created by gtliu on 11/6/12.
+//  Copyright (c) 2012 wicresoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (ImageText)
+-(void)setImage:(NSString *)imageName setTitle:(NSString *)title;
+-(void)changeImage:(NSString *)imageName;
+@end
